f584eca199416e8958ba3b1a06a8abff920b980e
Yasmeen Roumie <y4smeen@gmail.com>
2016-02-11 18:06:58 -0500

    customize

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -4,21 +4,6 @@
  * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
  */
 
-// starfield
-$(document).ready(function() {
-  $('#starfield').particleground({
-    dotColor: '#DDDDDD',
-    lineColor: 'rgba(255, 255, 255, .2)',
-    particleRadius: 3,
-    proximity: 80,
-    minRadiusPct: .4,
-    maxRadiusPct: 1.8,
-    minOpacity: .4,
-    maxOpacity: .8,
-    parallax: true
-  });
-});
-
 
 // Tooltip Init
 $(function() {
